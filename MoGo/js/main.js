1f73abda94729a8a4acf58f5d144dc1fe1650da9
black1ight <lasunaleksandr@gmail.com>
2020-03-28 16:21:50 +0200

    burger-scroll upd

--- MoGo/js/main.js
+++ MoGo/js/main.js
@@ -1,16 +1,16 @@
-$(function(){
+$(function () {
 
-    $('.accordion__header--1').click(function(){
+    $('.accordion__header--1').click(function () {
         $('.accordion__content-text--1').slideToggle();
         $('.accordion__header--1').toggleClass('active');
     });
 
-    $('.accordion__header--2').click(function(){
+    $('.accordion__header--2').click(function () {
         $('.accordion__content-text--2').slideToggle();
         $('.accordion__header--2').toggleClass('active');
     });
 
-    $('.accordion__header--3').click(function(){
+    $('.accordion__header--3').click(function () {
         $('.accordion__content-text--3').slideToggle();
         $('.accordion__header--3').toggleClass('active');
     });
@@ -21,75 +21,98 @@ $(function(){
         slidesToScroll: 1,
         responsive: [
             {
-              breakpoint: 480,
-              settings: {
-                arrows: false
-              }
+                breakpoint: 480,
+                settings: {
+                    arrows: false
+                }
             }
-          ]
+        ]
     });
 
-    $('.burger-btn').click(function(){
+    $('.burger-btn').click(function () {
         $('.burger-btn').toggleClass('active');
         $('.nav').slideToggle();
         $('.nav__link').removeClass('active');
     });
 
-    $('#about-link').click(function(){
+
+
+    $('#mine').click(function () {
+        $('.nav__link#mine').addClass('active');
+        var href = $(this).attr('href');
+        setTimeout(function () {
+            window.location = href,
+                $('.nav').css("display", "none");
+            $('.burger-btn').removeClass('active');
+        }, 1000);
+        return false;
+    });
+    $('#about-link').click(function () {
         $('.nav__link#about-link').addClass('active');
         var href = $(this).attr('href');
-        setTimeout(function() {
+        setTimeout(function () {
             window.location = href,
-            $('.nav').css("display", "none");
+                $('.nav').css("display", "none");
             $('.burger-btn').removeClass('active');
         }, 1000);
         return false;
-        $("body,html").animate({scrollTop: destination }, 800);
     });
-    $('#service-link').click(function(){
+    $('#service-link').click(function () {
         $('.nav__link#service-link').addClass('active');
         var href = $(this).attr('href');
-        setTimeout(function() {
+        setTimeout(function () {
             window.location = href,
-            $('.nav').css("display", "none");
+                $('.nav').css("display", "none");
             $('.burger-btn').removeClass('active');
         }, 1000);
         return false;
     });
-    $('#work-link').click(function(){
+    $('#work-link').click(function () {
         $('.nav__link#work-link').addClass('active');
         var href = $(this).attr('href');
-        setTimeout(function() {
+        setTimeout(function () {
             window.location = href,
-            $('.nav').css("display", "none");
+                $('.nav').css("display", "none");
             $('.burger-btn').removeClass('active');
         }, 1000);
         return false;
     });
-    $('#blog-link').click(function(){
+    $('#blog-link').click(function () {
         $('.nav__link#blog-link').addClass('active');
         var href = $(this).attr('href');
-        setTimeout(function() {
+        setTimeout(function () {
             window.location = href,
-            $('.nav').css("display", "none");
+                $('.nav').css("display", "none");
             $('.burger-btn').removeClass('active');
         }, 1000);
         return false;
     });
-    $('#contact-link').click(function(){
+    $('#contact-link').click(function () {
         $('.nav__link#contact-link').addClass('active');
         var href = $(this).attr('href');
-        setTimeout(function() {
+        setTimeout(function () {
             window.location = href,
-            $('.nav').css("display", "none");
+                $('.nav').css("display", "none");
             $('.burger-btn').removeClass('active');
         }, 1000);
         return false;
     });
+
+
+    var element = $('.about__item--2');
+    $counter = 0;
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop() + $(window).height();
+        var offset = element.offset().top;
+
+        if (scroll > offset && $counter == 0) {
+            $('.burger-btn').addClass('black');
+            $counter = 1;
+        }
+    });
+
 
 });
 
-setTimeout(function () {
-    // Ваш скрипт
-    }, 1000); // время в мс
-    
+
+
